2e85f138c753cfc58e4464294bb8e525296be54c
Bryan Grill <grillb@gmail.com>
2017-06-05 06:20:40 -0400

    add expo

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React. Hello!</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
--- src/app.native.js
+++ src/app.native.js
@@ -1,16 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo from 'expo';
+import React, { Component } from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { NavigationProvider, StackNavigation } from '@expo/ex-navigation';
+import { FontAwesome } from '@expo/vector-icons';
 
-export default class App extends React.Component {
+import Router from './navigation/Router';
+import cacheAssetsAsync from './utilities/cacheAssetsAsync';
+
+export default class AppContainer extends Component {
+  state = {
+    appIsReady: false,
+  };
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  async _loadAssetsAsync() {
+    try {
+      await cacheAssetsAsync({
+        images: [require('./assets/images/expo-wordmark.png')],
+        fonts: [
+          FontAwesome.font,
+          { 'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf') },
+        ],
+      });
+    } catch (e) {
+      console.warn(
+        'There was an error caching assets (see: main.js), perhaps due to a ' +
+          'network timeout, so we skipped caching. Reload the app to try again.',
+      );
+      console.log(e.message);
+    } finally {
+      this.setState({ appIsReady: true });
+    }
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Live change on this line.</Text>
-      </View>
-    );
+    if (this.state.appIsReady) {
+      return (
+        <View style={styles.container}>
+          <NavigationProvider router={Router}>
+            <StackNavigation
+              id="root"
+              initialRoute={Router.getRoute('rootNavigation')}
+            />
+          </NavigationProvider>
+
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          {Platform.OS === 'android' &&
+            <View style={styles.statusBarUnderlay} />}
+        </View>
+      );
+    } else {
+      return <Expo.AppLoading />;
+    }
   }
 }
 
@@ -18,7 +62,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  statusBarUnderlay: {
+    height: 24,
+    backgroundColor: 'rgba(0,0,0,0.2)',
   },
 });
+
+//Expo.registerRootComponent(AppContainer);
