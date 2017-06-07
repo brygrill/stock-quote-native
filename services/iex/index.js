afb772bb481de78072c5d9de23c9782bc4707d25
Bryan Grill <grillb@gmail.com>
2017-06-07 05:32:37 -0400

    start add iex service

---
+++ services/iex/index.js
@@ -0,0 +1,73 @@
+//const Gdax = require('gdax');
+const admin = require('firebase-admin');
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://ws-feed.gdax.com');
+const serviceAccount = require('./serviceAccountKey.json');
+
+let start = null;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://quoteum-fd8e3.firebaseio.com',
+});
+
+const db = admin.database();
+const ref = db.ref('stream');
+
+const updateStream = (coin, last) => {
+  ref.child(coin).set({ last });
+};
+
+// const websocket = new Gdax.WebsocketClient(['ETH-USD', 'BTC-USD']);
+// const gdaxSocket = () => {
+//   websocket.on('message', data => {
+//     if (data.type === 'match') {
+//       updateStream(data.product_id, data.price);
+//     }
+//   });
+// };
+
+// gdaxSocket();
+const pinger = () => {
+  return setInterval(() => {
+    ws.ping('keepalive');
+  }, 30000);
+};
+
+const elapsed = () => {
+  end = Date.now();
+  const elapsed = end - start;
+  console.log('Ended:', end);
+  console.log('Elapsed', elapsed);
+};
+
+query = {
+  type: 'subscribe',
+  product_ids: ['BTC-USD', 'ETH-USD'],
+};
+
+ws.on('open', function open() {
+  ws.send(JSON.stringify(query));
+  start = Date.now();
+  console.log('Started: ', start);
+  pinger();
+});
+
+ws.on('message', function incoming(data) {
+  const parsed = JSON.parse(data);
+  if (parsed.type === 'match') {
+    updateStream(parsed.product_id, parsed.price);
+  }
+});
+
+ws.on('close', function close() {
+  console.log('Disconnected');
+  elapsed();
+  clearInterval(pinger);
+});
+
+ws.on('error', function error(err) {
+  elapsed();
+  console.log('Error: ', err);
+});
