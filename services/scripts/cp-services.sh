fce17d1651c032eb2df4b2f6fa0c6a7664b29ca8
Bryan Grill <grillb@gmail.com>
2017-06-20 05:41:23 -0400

    update scripts

---
+++ services/scripts/cp-services.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcloud compute scp /Users/Bryan/Documents/Github/quote-stream/services grillb@quoteum-docker-1:/usr/docker-quoteum --recurse
---
+++ services/scripts/rm-node-modules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ../services
+
+find . -name a -type d -exec rm -rf {} \;
