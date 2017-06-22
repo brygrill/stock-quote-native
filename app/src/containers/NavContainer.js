2795ee027bcda2348a69ca581ebfa61d7d5777b0
Bryan Grill <grillb@gmail.com>
2017-06-21 23:19:29 -0400

    update nav

--- app/src/containers/NavContainer.js
+++ app/src/containers/NavContainer.js
@@ -4,11 +4,11 @@
 import React from 'react';
 import { Platform, StyleSheet, Text, StatusBar, View } from 'react-native';
 import { TabNavigator } from 'react-navigation';
-// $FlowFixMe
 import { Constants } from 'expo';
 import { Ionicons } from '@expo/vector-icons';
 
-import Realtime from '../screens/RealtimeScreen';
+// import CoinComponent from '../screens/CoinScreen';
+// import StockComponent from '../screens/StockScreen';
 
 // Set status bar to white
 StatusBar.setBarStyle('light-content', true);
@@ -47,33 +47,24 @@ const NavIcon = (props: { color: Object, icon: string, focused: Boolean }) =>
   />;
 
 // Render Realtime Screen
-const RealtimeScreen = (props: { navigation: Object }) =>
-  <Realtime banner="Realtime Tab" navigation={props.navigation} />;
+const CoinScreen = (props: { navigation: Object }) =>
+  // <CoinComponent banner="Coin Tab" navigation={props.navigation} />;
+  <ScreenContainer banner="Coin Tab" navigation={props.navigation} />;
 
-RealtimeScreen.navigationOptions = {
-  tabBarLabel: 'Realtime',
+CoinScreen.navigationOptions = {
+  tabBarLabel: 'Coins',
   tabBarIcon: (props: { tintColor: Object, focused: Boolean }) =>
     <NavIcon color={props.tintColor} icon="pulse" focused={props.focused} />,
 };
 
 // Render Data Screen
-const DataScreen = (props: { navigation: Object }) =>
-  <ScreenContainer banner="Data Tab" navigation={props.navigation} />;
+const StockScreen = (props: { navigation: Object }) =>
+  <ScreenContainer banner="Stock Tab" navigation={props.navigation} />;
 
-DataScreen.navigationOptions = {
-  tabBarLabel: 'Data',
+StockScreen.navigationOptions = {
+  tabBarLabel: 'Stocks',
   tabBarIcon: (props: { tintColor: Object, focused: Boolean }) =>
-    <NavIcon color={props.tintColor} icon="stats" focused={props.focused} />,
-};
-
-// Render News Screen
-const NewsScreen = (props: { navigation: Object }) =>
-  <ScreenContainer banner="News Tab" navigation={props.navigation} />;
-
-NewsScreen.navigationOptions = {
-  tabBarLabel: 'News',
-  tabBarIcon: (props: { tintColor: Object, focused: Boolean }) =>
-    <NavIcon color={props.tintColor} icon="paper" focused={props.focused} />,
+    <NavIcon color={props.tintColor} icon="speedometer" focused={props.focused} />,
 };
 
 // Render About Screen
@@ -93,18 +84,14 @@ AboutScreen.navigationOptions = {
 // Render Tabs
 const Tabs = TabNavigator(
   {
-    Realtime: {
-      screen: RealtimeScreen,
-      path: '',
+    Coins: {
+      screen: CoinScreen,
+      path: 'coins',
     },
-    // Data: {
-    //   screen: DataScreen,
-    //   path: 'data',
-    // },
-    // News: {
-    //   screen: NewsScreen,
-    //   path: 'news',
-    // },
+    Data: {
+      screen: StockScreen,
+      path: 'stocks',
+    },
     About: {
       screen: AboutScreen,
       path: 'settings',
