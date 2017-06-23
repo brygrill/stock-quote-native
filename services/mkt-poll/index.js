148a8f7d395284bcf4097363ae6e83b4a1f832e0
Bryan Grill <grillb@gmail.com>
2017-06-23 05:02:05 -0400

    update services

--- services/mkt-poll/index.js
+++ services/mkt-poll/index.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
 const axios = require('axios');
 const twilio = require('twilio');
+const moment = require('moment-timezone');
 const CronJob = require('cron').CronJob;
 const numeral = require('numeral');
+const isEmpty = require('lodash.isempty');
 
 const serviceAccount = require('./serviceAccountKey.json');
 const secrets = require('./secrets');
@@ -30,25 +32,42 @@ const instance = axios.create({
 });
 
 // update firebase
-const updateStream = (ticker, open, last, lastUpdatedAt, percDay, status, volume) => {
+const updateStream = (ticker, open, last, tickUpdatedAt, lastUpdatedAt, lastUpdatedAtHuman, percDay, status, volume) => {
   const target = ticker.toLowerCase();
   ref.child(target).update({
     open,
     last,
+    tickUpdatedAt,
     lastUpdatedAt,
+    lastUpdatedAtHuman,
     percDay,
     status,
     volume,
   });
 };
 
+// only send msg every 5 minutes
+const smsLastSent = (lastSent) => {
+  // duration will return minutes since last sent as a negative num
+  sinceLastSent = moment.duration(lastSent.diff(moment())).asMinutes();
+  if (sinceLastSent < -5) return true;
+  return false;
+}
+
 // send text
 const sms = body => {
-  return client.messages.create({
-    to,
-    from,
-    body,
-  });
+  // set lastSent to script start
+  lastSent = moment();
+  if (smsLastSent(lastSent)) {
+    // reset last sent
+    lastSent = moment();
+    // send msg
+    return client.messages.create({
+      to,
+      from,
+      body,
+    });
+  }
 };
 
 // format status
@@ -89,14 +108,17 @@ const fetchAllLast = () => {
   securities.map(item => {
     return fetchLast(item)
       .then(data => {
-        const last = numeral(data['03. Latest Price']).format('$0,0.00');
-        const open = numeral(data['04. Open (Current Trading Day)']).format('$0,0.00');
-        const lastUpdatedAt = data['11. Last Updated'];
-        const percDay = data['09. Price Change Percentage'];
-        const status = setDayStatus(percDay);
-        const volume = data['10. Volume (Current Trading Day)'];
-        console.log(item);
-        updateStream(item, open, last, lastUpdatedAt, percDay, status, volume);
+        if (!isEmpty(data)) {
+          const last = numeral(data['03. Latest Price']).format('$0,0.00');
+          const open = numeral(data['04. Open (Current Trading Day)']).format('$0,0.00');
+          const lastUpdatedAt = moment().tz(timeZone).format();
+          const lastUpdatedAtHuman = moment.duration(lastUpdatedAt).humanize(true)
+          const tickUpdatedAt = data['11. Last Updated'];
+          const percDay = data['09. Price Change Percentage'];
+          const status = setDayStatus(percDay);
+          const volume = data['10. Volume (Current Trading Day)'];
+          updateStream(item, open, last, tickUpdatedAt, lastUpdatedAt, lastUpdatedAtHuman, percDay, status, volume);
+        }
       })
       .catch(err => {
         console.log('Error: ', err); // eslint-disable-line
@@ -106,9 +128,9 @@ const fetchAllLast = () => {
 };
 
 // set cron job to run
-// poll every second during market hours
+// poll every few second during market hours
 const job = new CronJob({
-  cronTime: '0-59 * 08-17 * * 1-5',
+  cronTime: '*/4 * 08-17 * * 1-5',
   onTick() {
     return fetchAllLast();
   },
